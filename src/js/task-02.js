82cf5516084e4ab8d50fb68eff048d5f99b59c84
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-11-03 13:55:47 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector('#ingredients');
+// console.log(ingredientsEl);
+
+
+const ingredientsList = ingredients.map((ingridient) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingridient;
+  itemEl.classList.add('itemEl');
+  return itemEl;
+});
+ingredientsEl.append(...ingredientsList);

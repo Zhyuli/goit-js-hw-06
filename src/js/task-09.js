535b7dc1f72a8a8bc4ba6a39bf146b04b2023723
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-11-06 21:50:17 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -7,9 +7,14 @@ function getRandomHexColor() {
 // Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
 
 const btnChangeColor = document.querySelector('.change-color');
-console.log(btn);
+// console.log(btnChangeColor);
 
 const colorEl = document.querySelector('.color');
-console.log(btnChangeColor);
+// console.log(colorEl);
 
+const body = document.body;
+
+const changeColor = () => {
+  colorEl.textContent = body.style.backgroundColor = getRandomHexColor();
+};
 
